db0012228912bb408f5c977c18bfafff8766615f
david <daviddelacruzmorales42@gmail.com>
2025-03-05 00:29:00 -0600

    fixed table. it was added the function of status as atribute

--- restaurante/src/pages/home/Home.jsx
+++ restaurante/src/pages/home/Home.jsx
@@ -1,13 +1,92 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
 import './home.css'
 import reload from '../../assets/svg/reload.svg'
 import { useState } from 'react'
 function Home() {
   const [isLoading, setIsLoading] = useState(false);
+
   const pedidoStyle={
     fontSize : 'large',
     padding: '10px'
   }
+
+  const [pedidos, setPedidos] = useState([]);
+
+  useEffect(() =>{
+    const fakeData = [
+      {
+        id:1,
+        name:"Papas fritas",
+        price:100,
+        cantidad:1,
+        status:"pendiente"
+      },
+      {
+        id:2,
+        name:"Hamburguesa",
+        price:200,
+        cantidad:1,
+        status:"pendiente"
+      },
+      {
+        id:3,
+        name:"Refresco",
+        price:20,
+        cantidad:2,
+        status:"pendiente"
+      },{
+        id:4,
+        name:"Malteada",
+        price:60,
+        cantidad:1,
+        status:"pendiente"
+      }
+    ]
+    const fakeDataWithStatus = fakeData.map((item) => {
+      return {
+        ...item,
+        status: 'pendiente', 
+      };
+    });
+
+    setPedidos(fakeDataWithStatus);
+  },[])
+
+  const confirmOrder = (id) =>{
+    try{
+      const newArray = pedidos.map((item) =>{
+        if(item.id === id){
+          return {...item, status: 'confirmado'}
+        }
+        return item
+      })
+      setPedidos(newArray)
+      console.log("confirm order "+id)
+    }
+    catch(error){
+      console.log("error", error)
+    }
+    
+  }
+
+  const sendOrder = (id) =>{
+    try{
+      const newArray = pedidos.map((item) => {
+        if (item.id === id) {
+          return { ...item, status: "enviado" };
+        }
+        return item;
+      });
+      setPedidos(newArray);
+      console.log("send order "+ id)
+    }
+    catch(error){
+      console.log("error", error)
+  }
+}
+
+
+
   
   const loading = () =>{
     try{
@@ -23,20 +102,7 @@ function Home() {
       
     }
   }
-  const buttonPedido = () =>{
-    try{
-      setIsLoading(true);
-      
-    }
-    catch(error){
-      console.log("error", error)
-    }finally{
-      setTimeout(()=>{
-        setIsLoading(false);
-      },1000)
-      
-    }
-  }
+  
   return (
     <div className='canva'>
       {isLoading && (
@@ -66,26 +132,48 @@ function Home() {
         <table className='borderR'>
          
           <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-            <th></th>
-            <th></th>
+            <th className='box_table'>Nombre</th>
+            <th className='box_table'> Precio</th>
+            <th className='box_table'>Cantidad</th>
+            <th className='box_table'>Acción</th>
+            <th className='box_table'>Acción</th>
           </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-            <td><button>confirmar</button></td>
-            <td><button>mandar pedido</button></td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-            <td><button>confirmar</button></td>
-            <td><button>mandar pedido</button></td>
-          </tr>
+          {
+            pedidos.map((item)=>{
+              return(
+                <tr key={item.id}>
+                  <td className='box_table'>{item.name}</td>
+                  <td className='box_table'>{"$ "+item.price}</td>
+                  <td className='box_table'>{item.cantidad}</td>
+                  <td className='box_table_button'>
+                    
+                    {
+                      item.status === "pendiente" ?(
+                        <button className='button_action' onClick={() => {
+                          confirmOrder(item.id)
+                        }} >Confirmar</button>
+                      ) : (
+                        <button className='button_action' disabled>Pedido confirmado</button>
+                      )
+                    }
+                    
+                    </td>
+                    <td className='box_table_button'>
+                  {
+                    item.status === "confirmado" ?(
+                    <button className='button_action' onClick={() => {
+                        sendOrder(item.id)
+                    }} >Mandar pedido</button>
+                    ):(
+                      <button className='button_action' disabled>Pedido mandado</button>
+                    )
+                  }
+                  
+                  </td>
+                </tr>
+              )
+            })
+          }
          
         </table>
         </div>
@@ -93,5 +181,6 @@ function Home() {
     </div>
   )
 }
+
 
 export default Home
